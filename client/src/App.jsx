7e527eb6fe710342a70f1718971fc68ce3bb1563
bikram993298 <bikrambarman993298@gmail.com>
2024-10-06 16:48:19 +0530

    chnage cookie logic to

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -30,6 +30,7 @@ function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
+    const token=JSON.parse(sessionStorage.getItem("token"));
     dispatch(checkAuth());
   }, [dispatch]);
 
